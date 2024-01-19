790c20c5b20a8261ea5d00cac6132d2a579fbe05
allyksander <aleksandr.panchenko.1989@gmail.com>
2024-01-19 19:15:14 +0300

    feat: add and configurate ESLint and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcom to Joy Pizza React App
-        </p>
+        <p>Welcom to Joy Pizza React App</p>
         <a
           className="App-link"
           href="https://reactjs.org"
