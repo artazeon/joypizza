157b67bbd2efb4e26015829aad0f62f2874e58ea
artazeon <artazeon@gmail.com>
2024-01-26 18:44:09 +0300

    feat: JOY-14; Router pages: Home, Catalog, Card, Cart, 404

---
+++ src/pages/Cardpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Cardpage() {
+  return (
+    <div>
+      <h1>Card page</h1>
+      <p1>Card page...</p1>
+    </div>
+  );
+}
+
+export default Cardpage;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Homepage() {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <p>Home page...</p>
+    </div>
+  );
+}
+
+export default Homepage;
