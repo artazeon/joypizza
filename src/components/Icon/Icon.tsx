9c637fe421002b3a609d06ba975bcc7db71793a9
allyksander <aleksandr.panchenko.1989@gmail.com>
2024-02-21 14:55:41 +0300

    fix: fix Icon component and sprite.svg file

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,26 +1,9 @@
-/*
- * Source:
- * https://github.com/alexanderson1993/svg-icons-cli
- * To update icons - add new terminal and run script 'npm run build:icons'
- */
+import React from 'react';
+import svgSprite from './icons-sprite/sprite.svg';
+import { IIcon } from '@interfaces/Interfaces';
 
-import React, { type SVGProps } from 'react';
-import svgSprite from '../../icons-sprite/sprite.svg';
-import { type IconName } from '../../icons-sprite/name';
-
-interface IIcon extends SVGProps<SVGSVGElement> {
-  name: IconName;
-  size?: number;
-  className?: string;
-}
-
-export const Icon = ({ name, size = 24, className = '', ...rest }: IIcon) => (
-  <svg
-    className={`icon icon-${name}${className ? ` ${className}` : ''}`}
-    width={size}
-    height={size}
-    {...rest}
-  >
+export const Icon = ({ name, size = 24, classNames = [], ...rest }: IIcon) => (
+  <svg className={`icon icon-${name} ${classNames.join(' ')}`.trim()} {...rest}>
     <use xlinkHref={`${svgSprite}#${name}`} />
   </svg>
 );
--- src/interfaces/Interfaces.ts
+++ src/interfaces/Interfaces.ts
@@ -1,5 +1,12 @@
 import { ReactElement } from 'react';
+import { type IconName } from 'src/components/Icon/icons-sprite/name';
 
 export interface IChildren {
   children?: ReactElement | ReactElement[];
+}
+
+export interface IIcon {
+  name: IconName;
+  size?: number;
+  classNames?: string[];
 }
